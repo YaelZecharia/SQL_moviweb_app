94d30f4b4250b4f0d50ac460be6c5142a17dd5b0
unknown <yael.zecharia01@gmail.com>
2023-07-22 11:16:36 +0300

    error handling added

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
-from datamanager.json_data_manager import JSONDataManager
+from flask import Flask, render_template, request, redirect, url_for, flash
+from datamanager.json_data_manager import JSONDataManager, UserNotFoundError, UserAlreadyExists, MovieNotFound
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from the .env file
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 data_manager = JSONDataManager('users_data.json')  # creating a data_manager object
 
 
@@ -18,18 +23,25 @@ def list_users():
 
 @app.route('/users/<int:user_id>')
 def user_movies(user_id):
-    users = data_manager.get_all_users()
-    username = users[str(user_id)]['name']
-    movies = data_manager.get_user_movies(str(user_id))
-    return render_template('movies.html', movies=movies, username=username, user_id=user_id)
+    try:
+        username = data_manager.get_username_by_id(user_id)
+        movies = data_manager.get_user_movies(str(user_id))
+        return render_template('movies.html', movies=movies, username=username, user_id=user_id)
+    except UserNotFoundError as e:
+        flash(f"{e}")
+        return redirect(url_for("list_users"))
 
 
 @app.route('/add_user', methods=["POST", "GET"])
 def new_user():
     if request.method == "POST":
         user_name = request.form['name']
-        data_manager.add_user(user_name)
-        return redirect(url_for("list_users"))
+        try:
+            data_manager.add_user(user_name)
+            return redirect(url_for("list_users"))
+        except UserAlreadyExists as e:
+            flash(f"{e}")
+            return redirect(url_for("new_user"))
     else:
         return render_template('add_user.html')
 
@@ -37,9 +49,13 @@ def new_user():
 @app.route('/users/<int:user_id>/add_movie', methods=["POST", "GET"])
 def add_movie(user_id):
     if request.method == "POST":
-        title = request.form['name']
-        data_manager.add_movie(str(user_id), title)
-        return redirect(url_for("user_movies", user_id=user_id))
+        try:
+            title = request.form['name']
+            data_manager.add_movie(str(user_id), title)
+            return redirect(url_for("user_movies", user_id=user_id))
+        except Exception as e:
+            flash(f"{e}")
+            return redirect(url_for("add_movie", user_id=user_id))
     else:
         return render_template('add_movie.html', user_id=user_id)
 
@@ -53,17 +69,36 @@ def update_movie(user_id, movie_id):
             'rating': request.form['rating'],
             'director': request.form['director']
         }
-        data_manager.update_movie(str(user_id), str(movie_id), updated_movie_data)
-        return redirect(url_for("user_movies", user_id=user_id))
+        try:
+            data_manager.update_movie(str(user_id), str(movie_id), updated_movie_data)
+            return redirect(url_for("user_movies", user_id=user_id))
+        except UserNotFoundError as e:
+            flash(f"{e}")
+            return redirect(url_for("user_movies", user_id=user_id))
     else:
-        movie_info = data_manager.get_movie_by_id(user_id, movie_id)
-        return render_template('update_movie.html', user_id=user_id, movie_id=movie_id, movie_info=movie_info)
+        try:
+            movie_info = data_manager.get_movie_by_id(user_id, movie_id)
+            return render_template('update_movie.html', user_id=user_id, movie_id=movie_id, movie_info=movie_info)
+        except UserNotFoundError as e:
+            # Handle the case when the user or movie is not found
+            flash(f"{e}")
+            return redirect(url_for("user_movies", user_id=user_id))
 
 
 @app.route('/users/<user_id>/delete_movie/<movie_id>', methods=["POST"])
 def delete_movie(user_id, movie_id):
-    data_manager.delete_movie(str(user_id), str(movie_id))
-    return redirect(url_for("user_movies", user_id=user_id))
+    try:
+        data_manager.delete_movie(str(user_id), str(movie_id))
+        return redirect(url_for("user_movies", user_id=user_id))
+    except MovieNotFound as e:
+        # Handle the case when the movie is not found
+        flash(f"{e}")
+        return redirect(url_for("user_movies", user_id=user_id))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
--- datamanager/json_data_manager.py
+++ datamanager/json_data_manager.py
@@ -6,6 +6,25 @@ from helpers.api_helpers import MovieAPI
 movie_api = MovieAPI
 
 
+class UserNotFoundError(Exception):
+    pass
+
+
+class UserAlreadyExists(Exception):
+    pass
+
+
+class MovieAlreadyExists(Exception):
+    pass
+
+
+class ProblemFetchingInfo(Exception):
+    pass
+
+class MovieNotFound(Exception):
+    pass
+
+
 class JSONDataManager(DataManagerInterface):
     def __init__(self, filename):
         self.filename = filename
@@ -31,12 +50,23 @@ class JSONDataManager(DataManagerInterface):
 
         return users_data
 
+    def get_username_by_id(self, user_id):
+        """Input: user_id (str or int)
+           Output: Returns the username associated with the given user_id or None if not found"""
+        users_data = self.get_all_users()
+        if str(user_id) not in users_data:
+            raise UserNotFoundError(f"User ID {user_id} does not exist")
+        user_info = users_data.get(str(user_id))
+        if user_info:
+            return user_info.get("name")
+
     def get_user_movies(self, user_id):
         """ Input: This method takes one argument: user_id.
          Output: This method returns a list of movies for the given user.
          Each movie should be a dictionary with details about the movie. """
         users_data = self.get_all_users()
-
+        if str(user_id) not in users_data:
+            raise UserNotFoundError(f"User ID {user_id} does not exist")
         user_info = users_data.get(user_id)
         if user_info is None:
             return {}
@@ -48,13 +78,20 @@ class JSONDataManager(DataManagerInterface):
         users_data = self.get_all_users()
         # Check if user_id exists in users_data
         if str(user_id) not in users_data:
-            return f"User ID {user_id} does not exist"
+            raise UserNotFoundError(f"User ID {user_id} does not exist")
         movies = self.get_user_movies(user_id)
+        if str(movie_id) not in movies:
+            raise UserNotFoundError(f"Movie ID {movie_id} does not exist")
         movie_info = movies[str(movie_id)]
         return movie_info
 
-    def add_user(self, user_name):  # should this function be separated to 2 functions? what should it return?
+    def add_user(self, user_name):
         users_data = self.get_all_users()
+
+        # Check if the username already exists in users_data
+        for user_id, user_info in users_data.items():
+            if user_info["name"] == user_name:
+                raise UserAlreadyExists("Username already exists. Please choose a different username.")
 
         # Find the next free ID for the new user
         existing_ids = [int(user_id) for user_id in users_data.keys()]
@@ -76,19 +113,19 @@ class JSONDataManager(DataManagerInterface):
         users_data = self.get_all_users()
         # Check if user_id exists in users_data
         if str(user_id) not in users_data:
-            return f"User ID {user_id} does not exist"
+            raise UserNotFoundError(f"User ID {user_id} does not exist")
         movies = self.get_user_movies(user_id)
 
         for movie_id, movie_info in movies.items():
             if movie_info.get("name") == title:
                 # Movie already exists in the user's movie list
-                return "Movie already exists"
+                raise MovieAlreadyExists(f"Movie {title} already exists")
 
         # Fetch movie info from the API
         movie_info = MovieAPI.fetch_movie_info(title)
 
         if movie_info is None:
-            raise RuntimeError("There was a problem fetching movie info")
+            raise ProblemFetchingInfo("There was a problem fetching movie info")
 
         # Find the next free ID for the new movie
         existing_ids = [int(movie_id) for movie_id in movies.keys()]
@@ -109,7 +146,7 @@ class JSONDataManager(DataManagerInterface):
         users_data = self.get_all_users()
         # Check if user_id exists in users_data
         if str(user_id) not in users_data:
-            return f"User ID {user_id} does not exist"
+            raise UserNotFoundError(f"User ID {user_id} does not exist")
         movies = self.get_user_movies(user_id)
         movies[movie_id] = updated_movie_data
         users_data[str(user_id)]["movies"] = movies
@@ -121,8 +158,10 @@ class JSONDataManager(DataManagerInterface):
         users_data = self.get_all_users()
         # Check if user_id exists in users_data
         if str(user_id) not in users_data:
-            return f"User ID {user_id} does not exist"
+            raise UserNotFoundError(f"User ID {user_id} does not exist")
         movies = self.get_user_movies(user_id)
+        if not movies[movie_id]:
+            raise MovieNotFound("This movie does not exist")
         del movies[movie_id]
         users_data[str(user_id)]["movies"] = movies
         self.save_new_data(users_data)
